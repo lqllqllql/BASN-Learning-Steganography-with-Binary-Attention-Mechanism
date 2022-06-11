006f845de4f413034b4f79c3b9a72c3a98cd2a26
lqllqllql <103975573+lqllqllql@users.noreply.github.com>
2022-06-11 19:07:54 +0800

    Update variance_pool2d.py

--- source/Step-1-ITC-Image-Texture-Complexity/ops/variance_pool2d.py
+++ source/Step-1-ITC-Image-Texture-Complexity/ops/variance_pool2d.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.utils import _pair, _quadruple
 
-
+# 定义一个类
 class VariancePool2d(nn.Module):
   """ Variance Pool 2D module.
 
@@ -16,22 +16,28 @@ class VariancePool2d(nn.Module):
     padding: pool padding, int or 4-tuple (l, r, t, b) as in pytorch F.pad
     same: override padding and enforce same padding, boolean
   """
-
+  
+  # 定义类中的方法
   def __init__(self, kernel_size=3, stride=1, padding=0, same=False):
     super(VariancePool2d, self).__init__()
     self.kernel_size = kernel_size
     self.stride = stride
     self.padding = padding
     self.same = same
-
+ 
+    # _k,_s每个维度的内核索引
     self._k = _pair(kernel_size)
     self._s = _pair(stride)
     self._p = _quadruple(padding)  # convert to l, r, t, b
-
+    
+    #计算随机变量的期望值
     self.pool = nn.AvgPool2d(kernel_size=self.kernel_size, stride=self.stride)
-
+  
+  # 填充吗？
+  # x是二维随机变量，特征图的像素
   def _padding(self, x):
     if self.same:
+      # ih,iw是x的每个维度的索引
       ih, iw = x.size()[2:]
       if ih % self._s[0] == 0:
         ph = max(self._k[0] - self._s[0], 0)
@@ -41,6 +47,7 @@ class VariancePool2d(nn.Module):
         pw = max(self._k[1] - self._s[1], 0)
       else:
         pw = max(self._k[1] - (iw % self._s[1]), 0)
+       
       pl = pw // 2
       pr = pw - pl
       pt = ph // 2
