c392168c7e732e78943a87474ff2731d0a350029
lqllqllql <103975573+lqllqllql@users.noreply.github.com>
2022-06-13 16:17:27 +0800

    Update resnet.py

--- source/Step-2-MFD-Minimizing-Feature-Distortion/resnet.py
+++ source/Step-2-MFD-Minimizing-Feature-Distortion/resnet.py
@@ -12,10 +12,11 @@ def conv3x3(in_planes, out_planes, stride=1):
   """3x3 convolution with padding"""
   return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
 
-
+# 基本残差块：输入和输出通道相同
 class BasicBlock(nn.Module):
   expansion = 1
-
+  
+  # 默认：stride=1
   def __init__(self, inplanes, planes, stride=1, downsample=None):
     super(BasicBlock, self).__init__()
     self.conv1 = conv3x3(inplanes, planes, stride)
@@ -35,25 +36,31 @@ class BasicBlock(nn.Module):
 
     out = self.conv2(out)
     out = self.bn2(out)
-
+    
+    # 残差连接：downsample层的输入是x
+    # downsample：通道数增加，尺寸减小
     if self.downsample is not None:
       residual = self.downsample(x)
-
+    
+    # 残差公式
     out += residual
     out = self.relu(out)
 
     return out
 
-
+# 输入和输出通道不同
 class Bottleneck(nn.Module):
   expansion = 4
 
   def __init__(self, inplanes, planes, stride=1, downsample=None):
     super(Bottleneck, self).__init__()
+    # 1*1卷积
     self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
     self.bn1 = nn.BatchNorm2d(planes)
+    # 3*3卷积 stride=stride：2
     self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
     self.bn2 = nn.BatchNorm2d(planes)
+    # 1*1卷积
     self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, bias=False)
     self.bn3 = nn.BatchNorm2d(planes * self.expansion)
     self.relu = nn.ReLU(inplace=True)
@@ -85,12 +92,13 @@ class Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
   def __init__(self, block, layers, num_classes=1000):
-    self.inplanes = 64
+    self.inplanes = 64 # 输入通道数64
     super(ResNet, self).__init__()
     self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
     self.bn1 = nn.BatchNorm2d(64)
     self.relu = nn.ReLU(inplace=True)
     self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+    # 看_make_layer函数
     self.layer1 = self._make_layer(block, 64, layers[0])
     self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
     self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
@@ -100,13 +108,16 @@ class ResNet(nn.Module):
 
     for m in self.modules():
       if isinstance(m, nn.Conv2d):
+        # 初始化
         nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
       elif isinstance(m, nn.BatchNorm2d):
         nn.init.constant_(m.weight, 1)
         nn.init.constant_(m.bias, 0)
-
+  
+  # 搭建网络层
   def _make_layer(self, block, planes, blocks, stride=1):
     downsample = None
+    # 如果步幅不为1或者输入通道不等于输出通道*expansion：即进行下采样，通道数增加
     if stride != 1 or self.inplanes != planes * block.expansion:
       downsample = nn.Sequential(
           nn.Conv2d(
@@ -114,7 +125,8 @@ class ResNet(nn.Module):
           nn.BatchNorm2d(planes * block.expansion),
       )
 
-    layers = []
+    layers = [] #网络层
+    # 输入通道，输出通道，步幅，下采样
     layers.append(block(self.inplanes, planes, stride, downsample))
     self.inplanes = planes * block.expansion
     for _ in range(1, blocks):
